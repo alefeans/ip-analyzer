3a833a12b60691ea58d3eaf1d1cea484e4304bf8
alefeans <alefeans2@hotmail.com>
2022-08-05 21:26:31 -0300

    adding check on abuse ipdb api

--- server/src/resolvers/IPScoreResolver.ts
+++ server/src/resolvers/IPScoreResolver.ts
@@ -1,8 +1,32 @@
+import AbuseIPDBClient from "../services/AbuseIPDBClient"
+import is_ip_private from 'private-ip'
+
 type IP = string
+
+type IPVersion = 4 | 6
+
+type AbuseIPDBCheckResponse = {
+  data: {
+    ipAddress: string,
+    isPublic: boolean,
+    ipVersion: IPVersion,
+    isWhitelisted: boolean,
+    abuseConfidenceScore: number,
+    countryCode: string,
+    countryName?: string,
+    usageType: string,
+    isp: string,
+    domain: string,
+    hostnames: string[],
+    totalReports: number,
+    numDistinctUsers: number,
+    lastReportedAt: string
+  }
+}
 
 type IPScore = {
   ip: IP,
-  ipVersion: 4 | 6,
+  ipVersion: IPVersion,
   isPublic: boolean,
   country: string,
   reports: number,
@@ -12,16 +36,24 @@ type IPScore = {
   riskScore: number,
 }
 
-export const score = (args: { ip: IP }): IPScore => {
+export const score = async ({ ip }: { ip: IP }): Promise<IPScore> => {
+
+  if (is_ip_private(ip)) {
+    throw new Error('Private IP Address')
+  }
+
+  const requestIP = encodeURI(ip)
+  const checkRequest = `api/v2/check?ipAddress=${requestIP}`
+  const { data: { data } } = await AbuseIPDBClient.get<AbuseIPDBCheckResponse>(checkRequest)
   return {
-    ip: args.ip,
-    ipVersion: 4,
-    isPublic: true,
-    country: 'China',
-    reports: 10,
-    isp: 'Tencent Cloud Computing (Beijing) Co. Ltd',
-    usageType: 'Data Center/Web Hosting/Transit',
-    domain: 'tencent.com',
-    riskScore: 90
+    ip: ip,
+    ipVersion: data.ipVersion,
+    isPublic: data.isPublic,
+    country: data.countryName ? data.countryName : data.countryCode,
+    reports: data.totalReports,
+    isp: data.isp,
+    usageType: data.usageType,
+    domain: data.domain,
+    riskScore: data.abuseConfidenceScore
   }
 }
