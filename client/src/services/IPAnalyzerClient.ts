40a22259ccdd941e5820784aa28afef5fdefb190
alefeans <alefeans2@hotmail.com>
2022-08-05 21:26:11 -0300

    increasing api timeout and toast

--- client/src/services/IPAnalyzerClient.ts
+++ client/src/services/IPAnalyzerClient.ts
@@ -2,4 +2,4 @@ import { GraphQLClient } from 'graphql-request'
 
 const url = import.meta.env.VITE_IP_ANALYZER_ENDPOINT
 
-export const IPAnalyzerClient = new GraphQLClient(url, { timeout: 2000 })
+export const IPAnalyzerClient = new GraphQLClient(url, { timeout: 5000 })
