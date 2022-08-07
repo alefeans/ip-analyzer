f8ae15d5a83d7ccc1b16c226bd20eb5bf4182c6b
alefeans <alefeans2@hotmail.com>
2022-08-07 19:01:24 -0300

    removing return await

--- server/src/adapters/ip-scanner/check-ip-score.ts
+++ server/src/adapters/ip-scanner/check-ip-score.ts
@@ -2,5 +2,5 @@ import { IPScoreInput } from '../../core/types/ip-score'
 import { ipScanner as api } from './api'
 
 export const checkIPScore = async ({ ip }: IPScoreInput) => {
-  return await api.checkIPScore(ip)
+  return api.checkIPScore(ip)
 }
