c4ebfad90451138c100750cff9cb8276479308e2
alefeans <alefeans2@hotmail.com>
2022-11-15 22:35:35 -0300

    creating left menu

---
+++ client/src/components/MainMenu.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { ColorMode } from '@chakra-ui/react'
+import { FiMenu } from 'react-icons/fi'
+import { FaSun, FaMoon, FaQuestion } from 'react-icons/fa'
+import { IconButton, Menu, MenuButton, MenuItem, MenuList, useDisclosure } from '@chakra-ui/react'
+import AboutDrawer from './AboutDrawer'
+
+type CustomMenuProps = {
+  colorMode: ColorMode,
+  toggleColorMode: () => void
+}
+
+export default function CustomMenu({ colorMode, toggleColorMode }: CustomMenuProps) {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  return (
+    <>
+      <Menu closeOnSelect={false}>
+        <MenuButton
+          alignSelf='flex-end'
+          as={IconButton}
+          aria-label='Options'
+          icon={<FiMenu />}
+          variant='outline'
+          size='lg' />
+        <MenuList>
+          <MenuItem
+            icon={colorMode === 'light' ? <FaSun /> : <FaMoon />}
+            aria-label='Change color mode'
+            onClick={toggleColorMode}>
+            Change Theme
+          </MenuItem>
+          <MenuItem
+            icon={<FaQuestion />}
+            aria-label='About this page'
+            onClick={onOpen}>
+            About
+          </MenuItem>
+        </MenuList>
+      </Menu>
+      <AboutDrawer isOpen={isOpen} onClose={onClose} />
+    </>
+  )
+}
